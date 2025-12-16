9a8ec12c27bf2ae126cd393d0b70b7320167c067
Morgan Roman <morgan@turnkey.io>
2025-12-15 23:03:58 -0500

    remove multisig for this PR

--- src/TKGasStation/structs/TransientSignature.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.30;
-
-/// @notice Struct representing a transient signature for batch operations
-struct TransientSignature {
-    bytes signature;
-    uint8 index;  // expect the offchain interaction to provide the index via look up in the view function 
-}
