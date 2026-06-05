085c71be1785212294c643b832070af64e7d5ef3
Eric Velazquez <eric.velazquez@turnkey.io>
2026-06-04 17:20:19 -0700

    chore: rename IERC1721 to IERC1271 to match EIP-1271 standard (TXL-235)

--- src/TKGasStation/TKGasDelegate.sol
+++ src/TKGasStation/TKGasDelegate.sol
@@ -7,13 +7,13 @@ import {IBatchExecution} from "./interfaces/IBatchExecution.sol";
 import {ITKGasDelegate} from "./interfaces/ITKGasDelegate.sol";
 import {IERC721Receiver} from "./interfaces/IERC721Receiver.sol";
 import {IERC1155Receiver} from "./interfaces/IERC1155Receiver.sol";
-import {IERC1721} from "./interfaces/IERC1721.sol";
+import {IERC1271} from "./interfaces/IERC1271.sol";
 
 /// @title TKGasDelegate
 /// @notice Delegation contract for executing transactions with signature-based authorization
 /// @dev Implements EIP-712 for typed structured data signing, supporting standard execution and batch execution
 /// @custom:security-contact security@turnkey.com
-contract TKGasDelegate is EIP712, IERC1155Receiver, IERC721Receiver, IERC1721, ITKGasDelegate {
+contract TKGasDelegate is EIP712, IERC1155Receiver, IERC721Receiver, IERC1271, ITKGasDelegate {
     error DeadlineExceeded();
     error InvalidNonce();
     error NotSelf();
